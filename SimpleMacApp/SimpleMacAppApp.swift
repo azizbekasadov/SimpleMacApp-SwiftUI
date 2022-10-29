496f4ceb73f13fb5975fb16657d5ca9de2142448
Azizbek Asadov <azizbek1998@icloud.com>
2022-10-29 16:51:52 +0500

    Initial Commit

---
+++ SimpleMacApp/SimpleMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMacAppApp.swift
+//  SimpleMacApp
+//
+//  Created by Azizbek Asadov on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
